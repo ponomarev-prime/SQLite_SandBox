d403353156dedd45a8cfd370bfc9fb90e40f7318
Alex <ax.dev@yahoo.com>
2023-05-13 06:15:25 +0300

    nice!

--- flask_sqlalchem/app.py
+++ flask_sqlalchem/app.py
@@ -31,3 +31,62 @@ class Student(db.Model):
 def index():
     students = Student.query.all()
     return render_template('index.html', students=students)
+
+@app.route('/<int:student_id>/')
+def student(student_id):
+    student = Student.query.get_or_404(student_id)
+    return render_template('student.html', student=student)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        student = Student(firstname=firstname,
+                          lastname=lastname,
+                          email=email,
+                          age=age,
+                          bio=bio)
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    
+    return render_template('create.html')
+
+# ...
+
+
+@app.route('/<int:student_id>/edit/', methods=('GET', 'POST'))
+def edit(student_id):
+    student = Student.query.get_or_404(student_id)
+
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+
+        student.firstname = firstname
+        student.lastname = lastname
+        student.email = email
+        student.age = age
+        student.bio = bio
+
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', student=student)
+
+@app.post('/<int:student_id>/delete/')
+def delete(student_id):
+    student = Student.query.get_or_404(student_id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('index'))
